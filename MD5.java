f54be4f7edc8b1b1696e4e67426dd736201ce01b
Ibtasam kayani <Ibtasamkayani143@gmail.com>
2024-05-06 15:33:51 +0500

    Message Digest

---
+++ MD5.java
@@ -0,0 +1,36 @@
+package HackerRank;
+
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+import java.security.*;
+
+
+
+
+class Solution {
+    
+    public static String md5(String input) {
+           try{
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashText = no.toString(16);
+            while (hashText.length() < 32) {
+                hashText = "0" + hashText;
+            }
+            return hashText;
+        
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }}
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Input: ");
+        String input=sc.nextLine();
+        
+        System.out.println(md5(input));
+        sc.close();
+    }}
